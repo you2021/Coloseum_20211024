06a321abe1b5deafd76a8f808f2b1b03cf5391c2
jeong <dbwl232@naver.com>
2021-10-24 16:17:33 +0900

    화면단에서, 서버에 다녀오면 어떻게 할지 응답 처리 방안 코딩. - runOnuiThread {} 로 백그라운드가 UI를 적용

--- app/src/main/java/com/neppplus/coloseum_20211024/MainActivity.kt
+++ app/src/main/java/com/neppplus/coloseum_20211024/MainActivity.kt
@@ -3,9 +3,11 @@ package com.neppplus.coloseum_20211024
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.neppplus.coloseum_20211024.databinding.ActivityMainBinding
 import com.neppplus.coloseum_20211024.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,7 +30,24 @@ class MainActivity : AppCompatActivity() {
 //            ServerUtil.test(5)
 
         //서버의 로그인 기능에 전달
-            ServerUtil.postRequestLogin(inputEmail,inputPw)
+            ServerUtil.postRequestLogin(inputEmail,inputPw, object :ServerUtil.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+
+                    // 화면단에서 jsonObj 분석 -> 상황에 맞는 UI 처리.
+                    val code = jsonObj.getInt("code")
+
+                    // 로그인 성공시 -> 성공 토스트
+                    // 실패시 -> 왜 실패했는지 서버ㅏ 알려주는대로 토스트
+                     if(code == 200){
+                         runOnUiThread{
+                             Toast.makeText(this@MainActivity,"로그인 성공", Toast.LENGTH_SHORT).show()
+                         }
+                     }else{
+
+                     }
+
+                }
+            })
 
         }
     }
