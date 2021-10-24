42d93582c109d5c2f067bbea82fa132adfa0d85c
jeong <dbwl232@naver.com>
2021-10-24 14:41:20 +0900

    로그인 함수 작성중  - Request에 필요한 정보를 모두 세팅

--- app/src/main/java/com/neppplus/coloseum_20211024/MainActivity.kt
+++ app/src/main/java/com/neppplus/coloseum_20211024/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.neppplus.coloseum_20211024.databinding.ActivityMainBinding
+import com.neppplus.coloseum_20211024.utils.ServerUtil
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +24,9 @@ class MainActivity : AppCompatActivity() {
             // 가져온 이메일/비번을 로그로 출력
             Log.d("입력이메일", inputEmail)
             Log.d("입력비번", inputPw)
+
+            ServerUtil.test(5)
+
         }
     }
 }
