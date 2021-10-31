4b0ccc3ec6c8ddeff82a14a2423e731eac6a1486
jeong <dbwl232@naver.com>
2021-10-31 12:23:42 +0900

    로그인 성공시 - 닉네임 추출 토스트

--- app/src/main/java/com/neppplus/coloseum_20211024/MainActivity.kt
+++ app/src/main/java/com/neppplus/coloseum_20211024/MainActivity.kt
@@ -53,8 +53,18 @@ class MainActivity : BaseActivity() {
                     // 로그인 성공시 -> 성공 토스트
                     // 실패시 -> 왜 실패했는지 서버 알려주는대로 토스트
                     if(code == 200){
+//                        runOnUiThread{
+//                            Toast.makeText(mContext,"로그인 성공", Toast.LENGTH_SHORT).show()
+//                        }
+
+                        //응용문제, 로그인 한 사람의 닉네임 추출 -> "~~~님, 환영합니다."토스트 출력
+
+                        val dataObj = jsonObj.getJSONObject("data")
+                        val userObj = dataObj.getJSONObject("user")
+                        val nickname = userObj.getString("nick_name")
+
                         runOnUiThread{
-                            Toast.makeText(mContext,"로그인 성공", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(mContext, "${nickname}님 환영합니다.", Toast.LENGTH_SHORT).show()
                         }
                     }else{
                         // 서버가 알려주는 로그인실패 사유 파싱 ->토스트
