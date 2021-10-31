8496c2f1e9a3aca601e3f8c9e29453711e628f40
jeong <dbwl232@naver.com>
2021-10-31 15:58:02 +0900

    불러낸 정보 파싱 -> 닉네임 반영

--- app/src/main/java/com/neppplus/coloseum_20211024/MainActivity.kt
+++ app/src/main/java/com/neppplus/coloseum_20211024/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.coloseum_20211024.databinding.ActivityMainBinding
 import com.neppplus.coloseum_20211024.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -26,7 +27,19 @@ class MainActivity : BaseActivity() {
         // 연습 - 내 정보 받아오기 호출 => 닉네임 파싱, 텍스트뷰에 반영
 
 
-        ServerUtil.getRequestMyInfo(mContext,null)
+        ServerUtil.getRequestMyInfo(mContext,object : ServerUtil.JsonResponseHandler{
+            override fun onResponse(jsonObj: JSONObject) {
+                val dataObj = jsonObj.getJSONObject("data")
+                val userObj = dataObj.getJSONObject("user")
+                val nickname = userObj.getString("nick_name")
+
+                runOnUiThread{
+                    binding.nicknameTxt.text = nickname
+                }
+            }
+
+        })
+
 
     }
 
