ffeb991950a785847a2d35ecdff86bafc04b6f7a
정유진 <dbwl232@naver.com>
2021-11-07 11:34:32 +0900

    서버에서 데이터를 늦게 받아오는 경우 대비(비동기) - 목록 파싱 이후에 notifyDataSetChanged() 실행 조치

--- app/src/main/java/com/neppplus/coloseum_20211024/MainActivity.kt
+++ app/src/main/java/com/neppplus/coloseum_20211024/MainActivity.kt
@@ -87,6 +87,11 @@ class MainActivity : BaseActivity() {
                 }
 
                 // for문이 끝나면, mTopicList에 모든 토론 주제가 추가된 상태다.
+                // 어댑터가 변경사항을 감지하도록 처리하자 => 내용 반영 : UI 변경(백그라운드)
+
+                runOnUiThread{  // 백그라운드이기에 이 안에 하는것이 안전하다.
+                    mTopicAdapter.notifyDataSetChanged()
+                }
 
             }
 
