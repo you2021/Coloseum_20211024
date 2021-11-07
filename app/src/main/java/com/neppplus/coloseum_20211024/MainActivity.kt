8d26adea11e1d7e79d49574f0d969875e391f3db
정유진 <dbwl232@naver.com>
2021-11-07 11:21:09 +0900

    TopicData 클래스에 보조생성자 추가 - 코딩의 편의성 증대

--- app/src/main/java/com/neppplus/coloseum_20211024/MainActivity.kt
+++ app/src/main/java/com/neppplus/coloseum_20211024/MainActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.coloseum_20211024
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.neppplus.coloseum_20211024.databinding.ActivityMainBinding
 import com.neppplus.coloseum_20211024.datas.TopicData
@@ -53,6 +54,25 @@ class MainActivity : BaseActivity() {
     fun getTopicListFromServer(){
         ServerUtil.getRequestMainInfo(mContext, object  : ServerUtil.JsonResponseHandler{
             override fun onResponse(jsonObj: JSONObject) {
+
+                val dataObj = jsonObj.getJSONObject("data")
+                val topicsArr = dataObj.getJSONArray("topics")
+
+                // 0번째 주제 - topicsArr 갯수 직전까지를 반복
+                // 5개 주제 : 0 ~ 4번 주제까지 (5개)
+                for (index in 0 until topicsArr.length()){
+                    Log.d("반복문 확인", index.toString())
+
+                    // [] 안에 있는 {}를 순서대로 찾아내서 파싱 하자
+                    val topicObj = topicsArr.getJSONObject(index)
+
+                    Log.d("토론주제", topicObj.getString("title"))
+
+                    // topicObj는 토론 주제에 필요한 데이터를 들고 있다.
+                    // TopicData() 형태로 변환해주자 => 목록에 추가해주자.
+
+                    val topicData = TopicData()
+                }
 
             }
 
