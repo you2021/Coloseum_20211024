cab7335c08a12a9539cda004d581de5cdedb1b43
정유진 <dbwl232@naver.com>
2021-11-07 16:20:00 +0900

    댓글 상세 정보 파싱 준비 - 표현할 UI만 우선 작업

--- app/src/main/java/com/neppplus/coloseum_20211024/datas/ReplyData.kt
+++ app/src/main/java/com/neppplus/coloseum_20211024/datas/ReplyData.kt
@@ -2,6 +2,8 @@ package com.neppplus.coloseum_20211024.datas
 
 class ReplyData(var id:Int, var content:String) {
 
+//    var user : UserData 댓글을 적은 사람 정보 => 하위 데이터로
+
     constructor() :this(0,"내용없음")
 
 }
