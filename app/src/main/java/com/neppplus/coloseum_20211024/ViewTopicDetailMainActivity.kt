c8af303a9d7d4e0936299679b596b39de1f490f3
정유진 <dbwl232@naver.com>
2021-11-07 12:48:55 +0900

    실제 상세화면 주제 데이터 표시

--- app/src/main/java/com/neppplus/coloseum_20211024/ViewTopicDetailMainActivity.kt
+++ app/src/main/java/com/neppplus/coloseum_20211024/ViewTopicDetailMainActivity.kt
@@ -4,6 +4,7 @@ import android.database.DatabaseUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.neppplus.coloseum_20211024.databinding.ActivityViewTopicDetailMainBinding
 import com.neppplus.coloseum_20211024.datas.TopicData
 
@@ -26,6 +27,11 @@ class ViewTopicDetailMainActivity : BaseActivity() {
     override fun setValues() {
 
         mTopicData = intent.getSerializableExtra("topic") as TopicData
+
+        binding.topicTitleTxt.text = mTopicData.title
+
+        Glide.with(mContext).load(mTopicData.imageURL).into(binding.topicImg)
+
     }
 
 }
