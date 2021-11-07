9bc991dd885c3131350671a44af12a04f451db2b
정유진 <dbwl232@naver.com>
2021-11-07 10:37:39 +0900

    어댑터 기초 작업 중

---
+++ app/src/main/java/com/neppplus/coloseum_20211024/adapters/TopicAdapter.kt
@@ -0,0 +1,11 @@
+package com.neppplus.coloseum_20211024.adapters
+
+import android.content.Context
+import android.widget.ArrayAdapter
+import com.neppplus.coloseum_20211024.datas.TopicData
+
+class TopicAdapter(
+    val mContext:Context,
+    resId:Int,
+    val mList:List<TopicData>) :ArrayAdapter<TopicData>(mContext,resId,mList){
+}
