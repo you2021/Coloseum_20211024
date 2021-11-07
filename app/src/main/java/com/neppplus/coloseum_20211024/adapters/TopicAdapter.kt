11f2f38f8d41321371f762ac4db76abaa41e9b8f
정유진 <dbwl232@naver.com>
2021-11-07 10:42:59 +0900

    어댑터 기초 작업 마무리

--- app/src/main/java/com/neppplus/coloseum_20211024/adapters/TopicAdapter.kt
+++ app/src/main/java/com/neppplus/coloseum_20211024/adapters/TopicAdapter.kt
@@ -1,11 +1,26 @@
 package com.neppplus.coloseum_20211024.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.neppplus.coloseum_20211024.R
 import com.neppplus.coloseum_20211024.datas.TopicData
 
 class TopicAdapter(
     val mContext:Context,
-    resId:Int,
+    resId:Int,  // 일회용성으로 사용한다면 변수명을 붙이지 않는다.
     val mList:List<TopicData>) :ArrayAdapter<TopicData>(mContext,resId,mList){
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var temPow = convertView
+        if (temPow == null){
+            temPow = mInflater.inflate(R.layout.tocpic_list_item, null)
+        }
+        val row = temPow!!
+        return row
+    }
 }
