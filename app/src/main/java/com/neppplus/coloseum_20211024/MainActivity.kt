bf8bda43cc1184e12bae89ac39b029ecdadb1b88
정유진 <dbwl232@naver.com>
2021-11-07 10:51:08 +0900

    메인화면 정보를 서버에 요청하는 API 추가 - 파싱 작업 X

--- app/src/main/java/com/neppplus/coloseum_20211024/MainActivity.kt
+++ app/src/main/java/com/neppplus/coloseum_20211024/MainActivity.kt
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.coloseum_20211024.databinding.ActivityMainBinding
+import com.neppplus.coloseum_20211024.datas.TopicData
 import com.neppplus.coloseum_20211024.utils.ServerUtil
 import org.json.JSONObject
+import java.util.ArrayList
 
 class MainActivity : BaseActivity() {
 
     lateinit var binding : ActivityMainBinding
+
+    val mTopicList = ArrayList<TopicData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,23 +28,35 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
-        // 연습 - 내 정보 받아오기 호출 => 닉네임 파싱, 텍스트뷰에 반영
+        // /v2.main.info API가 토론 주제 목록을 내려줌
+        // 서버 호출 => 파싱해서 mTopicList를 채워주자
+        getTopicListFromServer()
 
 
-        ServerUtil.getRequestMyInfo(mContext,object : ServerUtil.JsonResponseHandler{
+
+
+
+//        // 연습 - 내 정보 받아오기 호출 => 닉네임 파싱, 텍스트뷰에 반영
+//        ServerUtil.getRequestMyInfo(mContext,object : ServerUtil.JsonResponseHandler{
+//            override fun onResponse(jsonObj: JSONObject) {
+//                val dataObj = jsonObj.getJSONObject("data")
+//                val userObj = dataObj.getJSONObject("user")
+//                val nickname = userObj.getString("nick_name")
+//                runOnUiThread{
+//                    binding.nicknameTxt.text = nickname
+//                }
+//            }
+//
+//        })
+    }
+
+    fun getTopicListFromServer(){
+        ServerUtil.getRequestMainInfo(mContext, object  : ServerUtil.JsonResponseHandler{
             override fun onResponse(jsonObj: JSONObject) {
-                val dataObj = jsonObj.getJSONObject("data")
-                val userObj = dataObj.getJSONObject("user")
-                val nickname = userObj.getString("nick_name")
 
-                runOnUiThread{
-                    binding.nicknameTxt.text = nickname
-                }
             }
 
         })
-
-
     }
 
 
